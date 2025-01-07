84c2620fff017cc52af6cdee2288a5485d333509
smooti <66232763+smooti@users.noreply.github.com>
2025-01-07 11:08:17 -0500

    Add files via upload

---
+++ addResources.bat
@@ -0,0 +1,31 @@
+player.additem 001BF72D 10000; Acid
+player.additem 001BF72E 10000; Adhesive
+player.additem 0006907A 10000; Aluminum
+player.additem 001BF72F 10000; Antiseptic
+player.additem 000AEC5C 10000; Asbestos
+player.additem 000AEC5B 10000; Ballistic Fiber
+player.additem 000AEC5D 10000; Bone
+player.additem 000AEC5E 10000; Ceramic
+player.additem 0006907B 10000; Circuitry
+player.additem 000AEC5F 10000; Cloth
+player.additem 00106D99 10000; Concrete
+player.additem 0006907C 10000; Copper
+player.additem 000AEC60 10000; Cork
+player.additem 0006907D 10000; Crystal
+player.additem 001BF730 10000; Fertilizer
+player.additem 00069087 10000; Fiber Optics
+player.additem 000AEC61 10000; Fiberglass
+player.additem 0006907E 10000; Gear
+player.additem 00069085 10000; Glass
+player.additem 000AEC62 10000; Gold
+player.additem 000AEC63 10000; Lead
+player.additem 000AEC64 10000; Leather
+player.additem 00069086 10000; Nuclear Material
+player.additem 001BF732 10000; Oil
+player.additem 0006907F 10000; Plastic
+player.additem 00106D98 10000; Rubber
+player.additem 00069081 10000; Screw
+player.additem 000AEC66 10000; Silver
+player.additem 00069082 10000; Spring
+player.additem 000731A4 10000; Steel
+player.additem 000731A3 10000; Wood
