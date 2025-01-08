842a14c839629f329475e5e315827775d9ccdb68
smooti <66232763+smooti@users.noreply.github.com>
2025-01-07 20:50:37 -0500

    Add files via upload

---
+++ addAmmo.bat
@@ -0,0 +1,25 @@
+player.additem 0001F66B 1000; 308
+player.additem 0004CE87 1000; .38
+player.additem 0009221C 1000; .44
+player.additem 0001F66A 1000; .45
+player.additem 0001F279 1000; .50
+player.additem 0001F66C 1000; 5mm
+player.additem 0001F278 1000; 5.56
+player.additem 0001F276 1000; 10mm
+:: player.additem 0001F673 1000; Shotgun Shell
+:: player.additem 000FE269 1000; Railway Spike
+:: player.additem 0018ABDF 1000; 2mm Electromagnetic Cartridge
+:: player.additem 0001DBB7 1000; Plasma Cartridge
+:: player.additem 0018ABE2 1000; Cryo Cell
+:: player.additem 000C1897 1000; Fusion Cell
+:: player.additem 00245D69 1000; Fusion Cell
+:: player.additem 000CAC78 1000; Flamer Fuel
+:: player.additem 001025AA 1000; Alien Blaster
+:: player.additem 000DF279 1000; Gamma
+:: player.additem 000FD11C 1000; Cannonball
+:: player.additem 000E6B2E 1000; Mini Nuke
+:: player.additem 000CABA3 1000; Missile
+:: player.additem 0001DBB7 1000; Plasma
+:: player.additem 000FE269 1000; Railway Spike
+:: player.additem 0001F673 1000; Shotgun Shell
+:: player.additem 00245D68 1000; Shotgun Shell
